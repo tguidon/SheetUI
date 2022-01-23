9a474bb8cf68d80908a77c7092c35aaa9b8b66d0
Taylor Guidon <tguidon@gmail.com>
2022-01-23 16:15:41 -0500

    Present Equatable Item
    commit 46fc86d53f79a3ae82264888c35289cd86978263
    Author: Taylor Guidon <tguidon@gmail.com>
    Date:   Sun Jan 23 16:15:00 2022 -0500
    
        Update README
    
    commit a19f665cb4884a4d7127a8f7c2e09c7347f26848
    Author: Taylor Guidon <tguidon@gmail.com>
    Date:   Sat Jan 22 23:15:18 2022 -0500
    
        working equatable
    
    commit 4b1775e0d91e5b224bdc67a5d7dd82ce084098ba
    Author: Taylor Guidon <tguidon@gmail.com>
    Date:   Sat Jan 22 22:43:13 2022 -0500
    
        better init
    
    commit 66bb0d8b5c72ae34cc824c369d3ed6183be88c77
    Author: Taylor Guidon <tguidon@gmail.com>
    Date:   Sat Jan 22 22:41:14 2022 -0500
    
        sorta working
    
    commit 32700381a178bb4ad607d7d26297f0a6aef5e74d
    Author: Taylor Guidon <tguidon@gmail.com>
    Date:   Sat Jan 22 22:35:06 2022 -0500
    
        Equatable
    
    commit 6b27ad07214daf51e540a2bf99f3adc3120fbaa0
    Author: Taylor Guidon <tguidon@gmail.com>
    Date:   Sat Jan 22 18:40:21 2022 -0500
    
        present item on z stack

--- Sources/SheetUI/SheetViewModifier.swift
+++ Sources/SheetUI/SheetViewModifier.swift
@@ -9,6 +9,76 @@ import Foundation
 import SwiftUI
 import UIKit
 
+public struct ItemSheetViewModifier<ContentView: View, Item: Equatable>: ViewModifier {
+    @Binding private var selectedItem: Item?
+    private let style: SheetViewStyle
+    private let onDismiss: (() -> Void)?
+    @ViewBuilder private let contentView: (Item) -> ContentView
+
+    @State private var sheetViewController: SheetViewController<ContentView>?
+
+    public init (
+        selectedItem: Binding<Item?>,
+        style: SheetViewStyle,
+        onDismiss: (() -> Void)?,
+        @ViewBuilder contentView: @escaping (Item) -> ContentView
+    ) {
+        self._selectedItem = selectedItem
+        self.style = style
+        self.onDismiss = onDismiss
+        self.contentView = contentView
+    }
+    
+    public func body(content: Content) -> some View {
+        ZStack {
+            content
+                .onChange(of: selectedItem, perform: updatePresentation)
+        }
+    }
+    
+    /// Update the presentation state of the `sheetViewController`
+    /// - Parameter isPresented: Presents the `sheetViewController` when set to `true`
+    private func updatePresentation(_ item: Item?) {
+        guard let presentingViewController = UIApplication.shared.presentingViewController else {
+            self.selectedItem = nil
+            return
+        }
+        
+        if let item = item {
+            sheetViewController = EquatableSheetViewController(
+                selectedItem: self.$selectedItem,
+                style: SheetViewStyle(),
+                content: self.contentView(item)
+            )
+            if let sheetViewController = self.sheetViewController {
+                presentingViewController.present(sheetViewController, animated: true)
+            }
+        } else {
+            sheetViewController?.dismiss(animated: true, completion: self.onDismiss)
+            sheetViewController = nil
+        }
+    }
+}
+
+public extension View {
+    func presentSheet<ContentView: View, Item: Equatable>(
+        selectedItem: Binding<Item?>,
+        style: SheetViewStyle = .standard,
+        onDismiss: (() -> Void)? = nil,
+        @ViewBuilder contentView: @escaping (Item) -> ContentView
+    ) -> some View {
+        self.modifier(
+            ItemSheetViewModifier(
+                selectedItem: selectedItem,
+                style: style,
+                onDismiss: onDismiss,
+                contentView: contentView
+            )
+        )
+    }
+}
+
+
 struct SheetViewModifier<ContentView: View>: ViewModifier {
     @Binding private var isPresented: Bool
     private let style: SheetViewStyle
@@ -50,7 +120,7 @@ struct SheetViewModifier<ContentView: View>: ViewModifier {
         }
         
         if isPresented {
-            sheetViewController = SheetViewController(
+            sheetViewController = IsPresentedSheetViewController(
                 isPresented: self.$isPresented,
                 style: self.style,
                 content: self.contentView
